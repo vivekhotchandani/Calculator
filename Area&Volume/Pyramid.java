a97b82ec1588b87cc59be0a820eb1da87554fc6c
vivekhotchandani <114832196+vivekhotchandani@users.noreply.github.com>
2024-03-09 17:41:51 +0530

    Update Pyramid.java

--- Area&Volume/Pyramid.java
+++ Area&Volume/Pyramid.java
@@ -1,3 +1,7 @@
+/* Name - Vivek Hotchandani
+PRN-22070126133
+Batch - AIML-B3
+*/
 public class Pyramid extends Shape implements Volume {
     private double baseLength;
     private double baseWidth;
